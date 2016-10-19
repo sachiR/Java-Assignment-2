eabd33e5475dd51320fdab1905260d2521f4d7ce
sachiR <sachini.perera@my.jcu.edu.au>
2016-10-20 00:20:03 +1000

    Green background and images in resources

--- src/com/test1/Main.java
+++ src/com/test1/Main.java
@@ -1,8 +1,26 @@
 package com.test1;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.Color;
+public class Main extends JFrame {
+    private JPanel options;
 
-public class Main {
+    public Main() {
+        super("The Mineral Super Trump Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container con = this.getContentPane();
+        con.setBackground(new Color(0, 153, 0));
 
-    public static void main(String[] args) {
-	// write your code here
+        JPanel welcomePanel = new JPanel();
+        welcomePanel.setLayout(new BorderLayout());
+        con.add(welcomePanel);
+    }
+
+    public static void main(String[] args)
+    {
+        Main cFrame = new Main();
+        cFrame.setSize(800,700);
+        cFrame.setVisible(true);
     }
 }
