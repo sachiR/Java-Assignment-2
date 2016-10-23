fa7f777dd3214033e86d2e43b328e386c0fd08a4
sachiR <sachini.perera@my.jcu.edu.au>
2016-10-23 10:16:00 +1000

    printing the random dealer edited

--- src/main/game/PlayPanel.java
+++ src/main/game/PlayPanel.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class PlayPanel extends JPanel{
     private int players;
 
-    //JLabel dealerLabel = new JLabel("The Dealer is "+ game.getDealerID());
+
 
     public PlayPanel(int numOfPlayers){
         setLayout(new FlowLayout());
@@ -14,6 +14,7 @@ public class PlayPanel extends JPanel{
         STGame game = new STGame(numOfPlayers);
         JLabel playLabel = new JLabel("The Number of players are " + numOfPlayers);
         add(playLabel);
-        //add(dealerLabel);
+        JLabel dealerLabel = new JLabel("The Dealer is "+ game.getDealerID());
+        add(dealerLabel);
     }
 }
